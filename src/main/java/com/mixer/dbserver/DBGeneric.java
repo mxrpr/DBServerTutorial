28bbf645d90a144fe8b6c9924fc146de19774ad9
mixer <ferenc.tollas@gmail.com>
2019-06-21 13:40:40 +0200

    DBGeneric.java, DBGenericServer.java: format code, add comments

--- src/main/java/com/mixer/dbserver/DBGeneric.java
+++ src/main/java/com/mixer/dbserver/DBGeneric.java
@@ -7,22 +7,88 @@ import java.io.Closeable;
 import java.io.IOException;
 
 public interface DBGeneric extends Closeable {
-
+	
+	/**
+	 * Use the specified table with the given schema
+	 * 
+	 * @param tableName Name of the table
+	 * @param schema Schema information in JSON
+	 * @param zclass Class of the stored object
+	 * @return Table
+	 * 
+	 * @throws DBException In case of any error, the API throws DBException
+	 */
     Table useTable(final String tableName,
                    final String schema,
                    final Class zclass) throws DBException;
 
+    /**
+     * Drop the currently used table
+     * 
+     * @return true if the deletion of the table is successfull
+     * 
+     * @throws DBException In case of any error, the API throws DBException
+     */
     boolean dropCurrentTable() throws DBException;
+    
+    /**
+     * Drop table
+     * 
+     * @param tableName
+     * @return true if the deletion of the table is successfull
+     * 
+     * @throws DBException In case of any error, the API throws DBException
+     */
 
     boolean dropTable(final String tableName) throws DBException;
 
+    /**
+     * Check whether a given table exists or not
+     * 
+     * @return true if the the table exists
+     * 
+     * @throws DBException In case of any error, the API throws DBException
+     */
+
     boolean tableExists(final String tableName);
 
+    /**
+     * Export given table to csv file
+     * 
+     * @param tableName Name of the table
+     * @param schema SChema in JSON format
+     * @param zclass Class of the stored object
+     * 
+     * @return String The generated string
+     * 
+     * @throws DBException  In case of any error, the API throws DBException
+     */
     String exportTableToCSV(final String tableName, final String schema, final Class zclass ) throws DBException;
 
+    /**
+     * Export currrently used table to csv file
+     * 
+     * @return String The generated string
+     * 
+     * @throws DBException  In case of any error, the API throws DBException
+     */    
     String exportCurrentTableToSCV() throws DBException;
 
     // List<String> getAvailableTables();
 
+    /**
+     * Close the database
+     * 
+     * @throws DBException  In case of any error, the API throws DBException
+     */
     void close() throws IOException;
+    
+    /**
+     * Runs a query against our database
+     * 
+     * @param queryString SQL query
+     * 
+     * @throws DBException  In case of any error, the API throws DBException
+     */
+    void runQuery(final String queryString) throws DBException;
 }
--- src/main/java/com/mixer/dbserver/DBGenericServer.java
+++ src/main/java/com/mixer/dbserver/DBGenericServer.java
@@ -12,128 +12,131 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public final class DBGenericServer implements DBGeneric {
-    // table name, reference to table instance
-    private Map<String, Table> tablePool;
-    private Table currentlyUsedTable = null;
-    private GenericIndexPool indexPool = null;
+	// table name, reference to table instance
+	private Map<String, Table> tablePool;
+	private Table currentlyUsedTable = null;
+	private GenericIndexPool indexPool = null;
 
-    public static final Logger LOGGER = Logger.getLogger("DBServer");
-    private static final String PROPERTY_FILE_NAME = "config.properties";
-    private static final String LOG_LEVEL = "LOG_LEVEL";
+	public static final Logger LOGGER = Logger.getLogger("DBServer");
+	private static final String PROPERTY_FILE_NAME = "config.properties";
+	private static final String LOG_LEVEL = "LOG_LEVEL";
 
-    public DBGenericServer() throws  DBException {
-        this.indexPool = new GenericIndexPool();
-        this.tablePool = new HashMap<>();
-        try {
-            this.initialise();
-        }catch(IOException e) {
-            throw new DBException(e.getMessage());
-        }
-    }
+	public DBGenericServer() throws DBException {
+		this.indexPool = new GenericIndexPool();
+		this.tablePool = new HashMap<>();
+		try {
+			this.initialise();
+		} catch (IOException e) {
+			throw new DBException(e.getMessage());
+		}
+	}
 
+	private void initialise() throws IOException {
 
+		Properties properties = new Properties();
+		try (FileInputStream fis = new FileInputStream(PROPERTY_FILE_NAME)) {
+			properties.load(fis);
+		}
 
-    private void initialise() throws  IOException {
+		boolean hasLogLevel = properties.containsKey(LOG_LEVEL);
+		if (!hasLogLevel)
+			LOGGER.setLevel(Level.SEVERE);
+		else {
+			String logLevel = (String) properties.get(LOG_LEVEL);
+			if (logLevel.equalsIgnoreCase("SEVERE"))
+				LOGGER.setLevel(Level.SEVERE);
+			else if (logLevel.equalsIgnoreCase("INFO"))
+				LOGGER.setLevel(Level.INFO);
+			else if (logLevel.equalsIgnoreCase("ALL"))
+				LOGGER.setLevel(Level.ALL);
+		}
+	}
 
-        Properties properties = new Properties();
-        try (FileInputStream fis = new FileInputStream(PROPERTY_FILE_NAME)) {
-            properties.load(fis);
-        }
+	@Override
+	public void close() throws IOException {
+		// cleanup the tablepool and close the tables
+		Set<String> _tableNames = this.tablePool.keySet();
+		try {
+			for (String tableName : _tableNames) {
+				this.tablePool.get(tableName).close();
+			}
+		} catch (DBException dbe) {
+			throw new IOException(dbe.getMessage());
+		}
+		// cleanup the indexpool and close the indexes
+		this.indexPool.close();
+	}
 
-        boolean hasLogLevel = properties.containsKey(LOG_LEVEL);
-        if (!hasLogLevel)
-            LOGGER.setLevel(Level.SEVERE);
-        else {
-            String logLevel = (String) properties.get(LOG_LEVEL);
-            if (logLevel.equalsIgnoreCase("SEVERE"))
-                LOGGER.setLevel(Level.SEVERE);
-            else if (logLevel.equalsIgnoreCase("INFO"))
-                LOGGER.setLevel(Level.INFO);
-            else if (logLevel.equalsIgnoreCase("ALL"))
-                LOGGER.setLevel(Level.ALL);
-        }
-    }
+	@Override
+	public Table useTable(final String tableName, final String schema, final Class zclass) throws DBException {
 
-    @Override
-    public void close() throws IOException {
-        // cleanup the tablepool and close the tables
-        Set<String> _tableNames = this.tablePool.keySet();
-        try {
-            for (String tableName : _tableNames) {
-                this.tablePool.get(tableName).close();
-            }
-        }catch(DBException dbe) {
-            throw new IOException(dbe.getMessage());
-        }
-        // cleanup the indexpool and close the indexes
-        this.indexPool.close();
-    }
+		// if the table is already loaded, then use it
+		if (this.tablePool.containsKey(tableName)) {
+			this.currentlyUsedTable = this.tablePool.get(tableName);
+		} else {
+			Table _table = new MxrTable(tableName, schema, zclass, this.indexPool);
+			this.tablePool.put(tableName, _table);
+			this.currentlyUsedTable = _table;
+		}
 
+		return this.currentlyUsedTable;
+	}
 
-    @Override
-    public Table useTable(final String tableName,
-                          final String schema,
-                          final Class zclass) throws DBException {
+	@Override
+	public boolean dropCurrentTable() throws DBException {
+		String _tableName = this.currentlyUsedTable.getTableName();
+		return this.dropTable(_tableName);
+	}
 
-        // if the table is already loaded, then use it
-        if(this.tablePool.containsKey(tableName)) {
-            this.currentlyUsedTable = this.tablePool.get(tableName);
-        }
-        else {
-            Table _table = new MxrTable(tableName, schema, zclass, this.indexPool);
-            this.tablePool.put(tableName, _table);
-            this.currentlyUsedTable = _table;
-        }
+	@Override
+	public boolean dropTable(final String tableName) throws DBException {
+		this.currentlyUsedTable.close();
+		this.tablePool.remove(tableName);
+		this.indexPool.deleteIndex(tableName);
+		this.currentlyUsedTable = null;
 
-        return this.currentlyUsedTable;
-    }
+		File file = new File((tableName));
+		if (!file.exists()) {
+			throw new DBException("Cannot delete table " + tableName);
+		}
+		return file.delete();
+	}
 
-    @Override
-    public boolean dropCurrentTable() throws DBException {
-        String _tableName = this.currentlyUsedTable.getTableName();
-        return this.dropTable(_tableName);
-    }
+	@Override
+	public boolean tableExists(final String tableName) {
+		return new File(tableName).exists();
+	}
 
-    @Override
-    public boolean dropTable(final String tableName) throws DBException{
-        this.currentlyUsedTable.close();
-        this.tablePool.remove(tableName);
-        this.indexPool.deleteIndex(tableName);
-        this.currentlyUsedTable = null;
+	@Override
+	public String exportTableToCSV(final String tableName, final String schema, final Class zclass) throws DBException {
+		Table _table = this.useTable(tableName, schema, zclass);
+		List<Object> result = _table.searchWithRegexp(".*");
+		StringBuilder stringBuilder = new StringBuilder(600);
+		for (Object obj : result) {
+			stringBuilder.append(((ICSVRepresentation) obj).toSCV());
+		}
 
-        File file = new File((tableName));
-        if(!file.exists()){
-            throw new DBException("Cannot delete table " + tableName);
-        }
-        return file.delete();
-    }
+		return stringBuilder.toString();
+	}
 
-    @Override
-    public boolean tableExists(final String tableName) {
-        return new File(tableName).exists();
-    }
+	@Override
+	public String exportCurrentTableToSCV() throws DBException {
+		List<Object> result = this.currentlyUsedTable.searchWithRegexp(".*");
+		StringBuilder stringBuilder = new StringBuilder(600);
+		for (Object obj : result) {
+			stringBuilder.append(((ICSVRepresentation) obj).toSCV());
+		}
 
-    @Override
-    public String exportTableToCSV(final String tableName, final String schema, final Class zclass ) throws DBException {
-        Table _table = this.useTable(tableName, schema, zclass);
-        List<Object> result = _table.searchWithRegexp(".*");
-        StringBuilder stringBuilder = new StringBuilder(600);
-        for ( Object obj : result) {
-            stringBuilder.append(((ICSVRepresentation)obj).toSCV());
-        }
+		return stringBuilder.toString();
 
-        return stringBuilder.toString();
-    }
-    @Override
-    public String exportCurrentTableToSCV() throws DBException{
-        List<Object> result = this.currentlyUsedTable.searchWithRegexp(".*");
-        StringBuilder stringBuilder = new StringBuilder(600);
-        for ( Object obj : result) {
-            stringBuilder.append(((ICSVRepresentation)obj).toSCV());
-        }
+	}
 
-        return stringBuilder.toString();
-
-    }
+	@Override
+	public void runQuery(final String queryString) throws DBException {
+		if (this.currentlyUsedTable == null) {
+			throw new DBException("No table is in use. Select a table!");
+		}
+		this.currentlyUsedTable.runQuery(queryString);
+	}
 
 }
