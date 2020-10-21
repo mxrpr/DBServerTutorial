7400b04dab300dfa27c76917a453f0d17b5cc56b
mixer <ferenc.tollas@gmail.com>
2020-10-21 19:10:21 +0200

    UtilTest.java: add tests for Leveinstein algo

--- src/test/java/UtilTest.java
+++ src/test/java/UtilTest.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class UtilTest {
+import com.mixer.util.Leveinshtein;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UtilTest {
+    @Test
+    public void testLeveinstein_0_distance() {
+        int result = Leveinshtein.leveinshteinDistance("John", "John");
+        Assert.assertEquals(0, result);
+    }
+
+    @Test
+    public void testLeveinstein_1_distance() {
+        int result = Leveinshtein.leveinshteinDistance("Cohn", "John");
+        Assert.assertEquals(1, result);
+    }
+    @Test
+    public void testLeveinstein_empty_strings() {
+        int result = Leveinshtein.leveinshteinDistance("", "");
+        Assert.assertEquals(0, result);
+    }
+
+    @Test
+    public void testLeveinstein_null_string() {
+        int result = Leveinshtein.leveinshteinDistance("", null);
+        Assert.assertEquals(-1, result);
+    }
 }
